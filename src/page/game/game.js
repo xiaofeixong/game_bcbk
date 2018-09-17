b3da88f320ebab5fedb530efb4ddf50e09dee940
xiaofeixong <jimforlx@gmail.com>
2018-09-17 20:45:35 +0800

    fix: 联调接口

--- src/page/game/game.js
+++ src/page/game/game.js
@@ -1,179 +1,280 @@
-$(function () {
-  var $gameZone = document.getElementById('gameZone');
-  var rowNum = 5 // 盒子行数
-  var reduce = '8%' // #gridA初始top值6
-  var step = 1.3 // 调节速度
-  var boxHeight = 1 / rowNum * 100 // 小格子高度
-  // 1. 生成交替的盒子id
-  var createGridId = (() => {
-    var gid = 'a'
-    return function () {
-      if (gid == 'gridA') gid = 'gridB'
-      else gid = 'gridA'
-      return gid
-    }
-  })()
-  // 2. 生成黑块数组
-  function createRandomIds() {
-    var arr = []
-    for (var i = 0; i < rowNum; i++) {
-      arr.push(Math.floor(Math.random() * 4) + i * 4)
-    }
-    console.log(arr, '  --  createRandomIds');
-    return arr
-  }
-
-  function createWhiteGrid() {
-    var whiteArr = []
-    for (var i = 0; i < rowNum; i++) {
-      var whiteNum = Math.floor(Math.random() * 4)
-      for (var j = 0; j <= whiteNum; j++) {
-        whiteArr.push(Math.floor(Math.random() * 4) + i * 4)
-      }
-    }
-    return whiteArr
-  }
-  // 3. 根据黑块数组+盒子id => 生成数组dom，拼到body中
-  var idsArr = []
-
-  function createBoxDom(identify) {
-    var tempArr = createRandomIds() // 黑块ids
-    var whiteArr = createWhiteGrid() // 白块ids
-
-    var gidStr = createGridId()
-    console.log(gidStr, '  --  createBoxDom');
-    var $div = document.createElement('div')
-    $div.id = gidStr
-
-    if (identify) {
-      tempArr.pop()
-    } else {
-      $div.style.top = reduce
-      $div.style.transform = 'translate3d(0,' + (-100) + '%,0)' // 负的BoxDom高度加上隐藏高度
+$(function() {
+    var $gameZone = document.getElementById('gameZone');
+    var rowNum = 5 // 盒子行数
+    var reduce = '8%' // #gridA初始top值6
+    var step = 1.3 // 调节速度
+    var boxHeight = 1 / rowNum * 100 // 小格子高度
+        // 1. 生成交替的盒子id
+    var createGridId = (() => {
+            var gid = 'a'
+            return function() {
+                if (gid == 'gridA') gid = 'gridB'
+                else gid = 'gridA'
+                return gid
+            }
+        })()
+        // 2. 生成黑块数组
+    function createRandomIds() {
+        var arr = []
+        for (var i = 0; i < rowNum; i++) {
+            arr.push(Math.floor(Math.random() * 4) + i * 4)
+        }
+        console.log(arr, '  --  createRandomIds');
+        return arr
     }
 
-    idsArr = tempArr.concat(idsArr)
-
-    var domString = ''
-    for (var i = 0; i < 4 * rowNum; i++) {
-      var isBlackClass = ''
-      if (tempArr.indexOf(i) > -1) {
-        isBlackClass = 'isBlack'
-      } else if (whiteArr.indexOf(i) > -1) {
-        isBlackClass = 'isWhite'
-      }
-      domString += '<div class="smallGrid ' + isBlackClass + '" id="grid_' + i + '"><div class="box"></div></div>'
+    function createWhiteGrid() {
+        var whiteArr = []
+        for (var i = 0; i < rowNum; i++) {
+            var whiteNum = Math.floor(Math.random() * 4)
+            for (var j = 0; j <= whiteNum; j++) {
+                whiteArr.push(Math.floor(Math.random() * 4) + i * 4)
+            }
+        }
+        return whiteArr
     }
-    $div.innerHTML = domString
-    $gameZone.appendChild($div);
-  }
-  // 4. 盒子移动动画函数
-  var distance = 0 // 总移动距离
-  var count = 0 // 总得分，即点击黑块次数
-  $('#gameCount').text(count)
-  var isDied = false
-  var myReq
+    // 3. 根据黑块数组+盒子id => 生成数组dom，拼到body中
+    var idsArr = []
 
-  function move() {
-    distance += step
-    // transform: translate3d(0,30%,0);
+    function createBoxDom(identify) {
+        var tempArr = createRandomIds() // 黑块ids
+        var whiteArr = createWhiteGrid() // 白块ids
 
-    var $gridA = document.getElementById('gridA')
-    var gridATop = $gridA.style.transform === '' ? 0 : Number($gridA.style.transform.split(',')[1].replace('%', ''))
-    gridATop += step
-    $gridA.style.transform = 'translate3d(0,' + gridATop + '%,0)'
-    if (gridATop >= boxHeight * rowNum) { // BoxDom初始显示高度
-      $gameZone.removeChild($gridA)
-      createBoxDom()
+        var gidStr = createGridId()
+        console.log(gidStr, '  --  createBoxDom');
+        var $div = document.createElement('div')
+        $div.id = gidStr
+
+        if (identify) {
+            tempArr.pop()
+        } else {
+            $div.style.top = reduce
+            $div.style.transform = 'translate3d(0,' + (-100) + '%,0)' // 负的BoxDom高度加上隐藏高度
+        }
+
+        idsArr = tempArr.concat(idsArr)
+
+        var domString = ''
+        for (var i = 0; i < 4 * rowNum; i++) {
+            var isBlackClass = ''
+            if (tempArr.indexOf(i) > -1) {
+                isBlackClass = 'isBlack'
+            } else if (whiteArr.indexOf(i) > -1) {
+                isBlackClass = 'isWhite'
+            }
+            domString += '<div class="smallGrid ' + isBlackClass + '" id="grid_' + i + '"><div class="box"></div></div>'
+        }
+        $div.innerHTML = domString
+        $gameZone.appendChild($div);
+    }
+    // 4. 盒子移动动画函数
+    var distance = 0 // 总移动距离
+    var count = 0 // 总得分，即点击黑块次数
+    $('#gameCount').text(count)
+    var isDied = false
+    var myReq
+
+    function move() {
+        distance += step
+            // transform: translate3d(0,30%,0);
+
+        var $gridA = document.getElementById('gridA')
+        var gridATop = $gridA.style.transform === '' ? 0 : Number($gridA.style.transform.split(',')[1].replace('%', ''))
+        gridATop += step
+        $gridA.style.transform = 'translate3d(0,' + gridATop + '%,0)'
+        if (gridATop >= boxHeight * rowNum) { // BoxDom初始显示高度
+            $gameZone.removeChild($gridA)
+            createBoxDom()
+        }
+
+        var $gridB = document.getElementById('gridB')
+        var gridBTop = Number($gridB.style.transform.split(',')[1].replace('%', ''))
+        gridBTop += step
+        $gridB.style.transform = 'translate3d(0,' + gridBTop + '%,0)'
+        if (gridBTop >= boxHeight * rowNum) {
+            $gameZone.removeChild($gridB)
+            createBoxDom()
+        }
+        if (distance >= count * boxHeight || isDied) {
+            // console.log(count, gridATop, gridBTop, 'zzzzzzzzzzz');
+            return
+        }
+        myReq = requestAnimationFrame(move)
     }
 
-    var $gridB = document.getElementById('gridB')
-    var gridBTop = Number($gridB.style.transform.split(',')[1].replace('%', ''))
-    gridBTop += step
-    $gridB.style.transform = 'translate3d(0,' + gridBTop + '%,0)'
-    if (gridBTop >= boxHeight * rowNum) {
-      $gameZone.removeChild($gridB)
-      createBoxDom()
+    // 5.格子点击事件
+    $('#gameZone').on('touchstart', function(event) {
+        if (isDied) return
+
+        var event = event || window.event;
+        var target = event.target || event.srcElement;
+        if (target.className.indexOf('smallGrid') === -1) {
+            target = target.parentNode
+        }
+        if (target.className.indexOf('smallGrid') === -1) return
+
+        // 当前点击的格子是最后一个黑格
+        if (Number(target.id.replace('grid_', '')) === idsArr.pop()) {
+            target.style.opacity = 0.5
+            count++
+            if (count === 1) {
+                run('10:00')
+            }
+            $(target).removeClass('clickme')
+            if (count <= 3) {
+                clickme()
+            }
+            $('#gameCount').text(count)
+                // console.log(count, 'addEventListener');
+            if (myReq) cancelAnimationFrame(myReq)
+            move()
+        } else {
+            target.className = 'smallGrid error'
+            stop()
+            console.log('GG');
+        }
+    })
+
+    // 6.倒计时
+    var countDownTimer
+
+    function run(startTime) { //定义时间函数，让秒表每100ms变化一次
+        function onTimer() {
+            //如果倒计时结束清除时间函数
+            if (startTime == '00:00') {
+                stop()
+                startTime = "00:01"; //(清除时间函数后还是会执行一次 所以多给一个10ms再动态赋值)
+            }
+
+            var hms = new String(startTime).split(":"); //以:作为分隔符号取字符串内的数据
+            var ms = new Number(hms[1]); //给每个数据定义对象
+            var s = new Number(hms[0]);
+
+            ms -= 1; //每次执行ms减10
+
+            //判断时间并进行变化
+            if (ms < 0) {
+                ms = 99;
+                s -= 1;
+            }
+            var ms = ms < 10 ? ("0" + ms) : ms; //如果出现个位数给个位数前面添加0
+            var ss = s < 10 ? ("0" + s) : s;
+            startTime = ss + ":" + ms;
+            $('#gameTime').text(startTime)
+        }
+        countDownTimer = setInterval(onTimer, 10); //100ms的定时器
     }
-    if (distance >= count * boxHeight || isDied) {
-      // console.log(count, gridATop, gridBTop, 'zzzzzzzzzzz');
-      return
+
+    // 7.游戏结束，弹出弹框
+    var currentRanking = ''
+
+    function stop() {
+        isDied = true
+        clearInterval(countDownTimer);
+        setTimeout(function() {
+            $('#gamePopupAvatar').attr('src', userInfo.headimgurl)
+            $('#popupScore').show()
+            toolAjaxGet('activeweb.restful.addIntegralRecord', {
+                url: house_url + '/activeweb.restful.addIntegralRecord',
+                openid: userInfo.openid,
+                nickname: userInfo.nickname,
+                headimgurl: userInfo.headimgurl,
+                integral: count
+            }, function(res) {
+                console.log(res, '新增用户');
+                $('#userScore').text(count);
+                // TODO: 排名
+                currentRanking = res.ranking
+                console.log(currentRanking, 'currentRanking');
+                $('#userRanking').text(res.ranking);
+            })
+        }, 500)
     }
-    myReq = requestAnimationFrame(move)
-  }
 
-  // 5.格子点击事件
-  $('#gameZone').on('touchstart', function (event) {
-    if (isDied) return
-
-    var event = event || window.event;
-    var target = event.target || event.srcElement;
-    if (target.className.indexOf('smallGrid') === -1) {
-      target = target.parentNode
+    // 8.找到最近一个格子
+    function clickme() {
+        $('#grid_' + idsArr[idsArr.length - 1]).addClass('clickme')
     }
-    if (target.className.indexOf('smallGrid') === -1) return
 
-    // 当前点击的格子是最后一个黑格
-    if (Number(target.id.replace('grid_', '')) === idsArr.pop()) {
-      target.style.opacity = 0.5
-      count++
-      if (count === 1) {
-        run('10:00')
-      }
-      $('#gameCount').text(count)
-      // console.log(count, 'addEventListener');
-      if (myReq) cancelAnimationFrame(myReq)
-      move()
-    } else {
-      target.className = 'smallGrid error'
-      stop()
-      // todo: 游戏结束，弹出遮罩
-      console.log('GG');
+    window.onload = () => {
+        createBoxDom('first')
+        createBoxDom()
+        clickme()
     }
-  })
 
-  // 6.倒计时
-  var countDownTimer
 
-  function run(startTime) { //定义时间函数，让秒表每100ms变化一次
-    function onTimer() {
-      //如果倒计时结束清除时间函数
-      if (startTime == '00:00') {
-        stop()
-        startTime = "00:01"; //(清除时间函数后还是会执行一次 所以多给一个10ms再动态赋值)
-      }
 
-      var hms = new String(startTime).split(":"); //以:作为分隔符号取字符串内的数据
-      var ms = new Number(hms[1]); //给每个数据定义对象
-      var s = new Number(hms[0]);
 
-      ms -= 1; //每次执行ms减10
+    // TODO: 设置用户头像
+    $('#userAvatar').attr('src', userInfo.headimgurl)
 
-      //判断时间并进行变化
-      if (ms < 0) {
-        ms = 99;
-        s -= 1;
-      }
-      var ms = ms < 10 ? ("0" + ms) : ms; //如果出现个位数给个位数前面添加0
-      var ss = s < 10 ? ("0" + s) : s;
-      startTime = ss + ":" + ms;
-      $('#gameTime').text(startTime)
-    }
-    countDownTimer = setInterval(onTimer, 10); //100ms的定时器
-  }
+    $('#playAgain').click(function(e) {
+        location.reload()
+    });
+    // 排行榜
 
-  // 7.游戏结束，弹出弹框
-  function stop() {
-    isDied = true
-    clearInterval(countDownTimer);
-    setTimeout(function () {
-      $('#popupScore').show()
-      $('#userScore').text(count)
-    }, 500)
-  }
 
-  window.onload = () => {
-    createBoxDom('first')
-    createBoxDom()
-  }
+    $('#ranking').click(function() {
+        toolAjaxGet('activeweb.restful.getRanking', {
+            url: house_url + '/activeweb.restful.getRanking'
+        }, function(res) {
+            console.log(res.data, '排行榜数据');
+            $('#currentRanking').text(currentRanking);
+
+            // TODO: 获取排行榜数据，渲染
+            var domString = ''
+            res.data.forEach(function(el, index) {
+                domString += '<ul class="game__ranking-item"><li>No.' + (index +
+                        1) +
+                    '</li><li><img src=" ' + el.headimgurl +
+                    '" class="game__ranking-avatar"></li><li>' + el.nickname +
+                    '</li><li>' +
+                    el.integral +
+                    '</li></ul>'
+            });
+            $('#rankingTable').append(domString);
+            $('#popupRanking').show();
+        })
+    });
+    $('#rankingMarkClose').click(function() {
+        $('#popupRanking').hide()
+    });
+
+    // 领奖台 -- submit
+    $('#award').click(function(e) {
+        $('#popupAward').show()
+        $('#popupScore').hide()
+    });
+    $('#submit').click(function() {
+        var name = $('#awardName').val()
+        var phone = $('#awardPhone').val()
+        if (!name.length) {
+            toolDialog('姓名不能为空！')
+            return
+        } else if (name.length > 12) {
+            toolDialog('姓名在12个字以内！')
+            return
+        }
+        if (!/^1\d{10}$/.test(phone.replace(/\s+/g, ''))) {
+            toolDialog('请输入正确的手机号！')
+            return
+        }
+        // TODO: 提交表单
+        toolAjaxGet('servicecenterapi.order.addorder', {
+            gid: '5b9f696539b54e466c43cad6',
+            userName: name,
+            phone: phone,
+            userid: '5b9f696539b54e466c43cad6',
+            orderUrl: 'no',
+        }, function(res) {
+            console.log(res);
+            toolDialog('提交成功！', function() {
+                location.reload()
+            })
+        })
+    });
+    $('#awardMarkClose').click(function() {
+        $('#popupAward').hide()
+        $('#popupScore').show()
+    });
 })
--- src/page/tools/config.js
+++ src/page/tools/config.js
@@ -1,45 +1,141 @@
-// var scweb_router_url = 'http://test.scweb.fdc.com.cn/router/rest' // test
-var scweb_router_url = 'http://gw.fdc.com.cn/router/rest' // line
+// test
+var gw_url = 'http://sandbox.gw.fdc.com.cn/router/rest'
+var house_url = 'http://house.m.pre.fdc.com.cn'
+
+// line
+// var gw_url = 'http://gw.fdc.com.cn/router/rest'
+var queryString = {
+    stringify: function(params) {
+        var search = []
+        for (var key in params) {
+            if (params.hasOwnProperty(key)) {
+                search.push(key + '=' + params[key])
+            }
+        }
+        return search.join('&')
+    }
+}
 
 function toolAjaxGet(method, params, callback) {
-  var requrl = params.url
-  var jsonpcb = params.jsonpcb
-  var success = successStr(method)
-  params.method = method
-  delete params.url
-  delete params.jsonpcb
-  $.ajax({
-    type: "get",
-    url: requrl || scweb_router_url,
-    data: params,
-    dataType: "jsonp",
-    jsonp: jsonpcb || "uccallback",
-    success: function (res) {
-      if (callback) {
-        callback(res[success])
-      }
-    }
-  })
+    var requrl = params.url || gw_url
+    var jsonpcb = params.jsonpcb
+    var success = successStr(method)
+    console.log(success, 'toolAjaxGet');
+    params.method = method
+    delete params.url
+    delete params.jsonpcb
+    requrl = requrl + '?' + queryString.stringify(params)
+    $.get(requrl, function(res) {
+        if (res) {
+            res = JSON.parse(res)
+            callback(res[success])
+        }
+    })
 }
+// function toolAjaxGet(method, params, callback) {
+//     var requrl = params.url || gw_url
+//     var jsonpcb = params.jsonpcb
+//     var success = successStr(method)
+//     console.log(success, 'toolAjaxGet');
+//     params.method = method
+//     delete params.url
+//     delete params.jsonpcb
+//     $.ajax({
+//         type: "get",
+//         url: requrl || scweb_router_url,
+//         data: params,
+//         dataType: "jsonp",
+//         jsonp: jsonpcb || "uccallback",
+//         success: function(res) {
+//             if (callback) {
+//                 callback(res[success])
+//             }
+//         },
+//         error: function(res) {
+//             console.log(res, params.method, 'ajax error');
+//         }
+//     })
+// }
 
 function successStr(method) {
-  var arr = method.split('.')
-  arr.splice(0, 1)
-  arr.push('response')
-  return arr.join('_')
+    var arr = method.split('.')
+    arr.splice(0, 1)
+    arr.push('response')
+    return arr.join('_')
 }
+
+
 
 // 弹框提示
 function toolDialog(msg, callback) {
-  $('#dialogTip').text(msg).show()
-  setTimeout(function () {
-    $("#dialogTip").animate({
-        opacity: 0,
-      }, 1000,
-      'ease-out',
-      function () {
-        $('#dialogTip').hide().css('opacity', 1)
-        callback()
-      })
-  }, 1000);
+    $('#dialogTip').text(msg).show()
+    setTimeout(function() {
+        $("#dialogTip").animate({
+                opacity: 0,
+            }, 1000,
+            'ease-out',
+            function() {
+                $('#dialogTip').hide().css('opacity', 1)
+                callback()
+            })
+    }, 1000);
 }
+
+// 获取cookie
+var docCookies = {
+    getItem: function(sKey) {
+        return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+    },
+    setItem: function(sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+        if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) {
+            return false;
+        }
+        sPath = sPath || '/';
+        sDomain = sDomain || '.fdc.com.cn';
+        var sExpires = "";
+        if (vEnd) {
+            switch (vEnd.constructor) {
+                case Number:
+                    sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+                    break;
+                case String:
+                    sExpires = "; expires=" + vEnd;
+                    break;
+                case Date:
+                    sExpires = "; expires=" + vEnd.toUTCString();
+                    break;
+            }
+        }
+        document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+        return true;
+    },
+    removeItem: function(sKey, sPath, sDomain) {
+        if (!sKey || !this.hasItem(sKey)) {
+            return false;
+        }
+        document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
+        return true;
+    },
+    hasItem: function(sKey) {
+        return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+    },
+    keys: /* optional method: you can safely remove it! */ function() {
+        var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+        for (var nIdx = 0; nIdx < aKeys.length; nIdx++) {
+            aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]);
+        }
+        return aKeys;
+    }
+};
+
+// TODO: openid  nickname headimgurl
+var userInfo = {}
+userInfo.openid = docCookies.getItem('openid')
+
+if (!userInfo.openid) {
+    window.location.href = 'http://house.m.pre.fdc.com.cn/weixinServlet?sourceUrl=' + window.location.href
+} else {
+    userInfo.headimgurl = docCookies.getItem('headimgurl')
+    userInfo.nickname = docCookies.getItem('nickname')
+}
+console.log(JSON.stringify(userInfo), 'get in');
